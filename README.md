139ae933519940f852b34362102174327777b102
Christoph Heindl <christoph.heindl@gmail.com>
2021-08-17 20:18:11 +0200

    updated docs and impl

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bone-solve-ik
+
+This work solves the problem of finding optimal parameters (joint angles) of a given kinematic to best align with set of noisy anchor points. This problem arises, for example, when trying to fit a human kinematic model to a set of 3D human joint positions - a common output of many 3D human pose approaches. Furthermore, these predictions are noisy, causing bone lengths to vary over time, rendering  heuristic approaches to kinematic alignment sub-optimal.
+
+This library frames the alignment problem as an optimization task, which minimizes the geometric distance between kinematic joint locations and corresponding anchor points. In this respect, the task is similar to inverse kinematics (IK) in robotics, but not quite. The main differences are
+ 1. In IK, the kinematic parameters are usually solved so that the end effector matches a certain target position. Hence, the solutions in IK are generally not unique - more than one configuration may place the end-effector in the target pose. In contrast, **bone-solve-ik** considers target poses for intermediate joints as well. These anchor points constrain the subjective function more than vanilla IK does.
+ 2. IK is concerned with finding a target pose that consists of location and orientation information. In contrast, **bone-solve-ik** only considers target location information.
--- boneik/bone.py
+++ boneik/bone.py
@@ -7,30 +7,39 @@ import torch.optim as optim
 from torch.nn.parameter import Parameter
 import torch.distributions as dist
 import torch.distributions.constraints as constraints
-import matplotlib.pyplot as plt
 
 
 class RotDOF(torch.nn.Module):
     def __init__(
         self,
         angle: float = 0.0,
-        angle_constraint: Tuple[float, float] = (-np.pi, np.pi),
-        enabled: bool = False,
+        constraint_interval: Tuple[float, float] = (-np.pi, np.pi),
+        unlocked: bool = False,
     ):
         super().__init__()
         self.constr = dist.transform_to(
-            constraints.half_open_interval(angle_constraint[0], angle_constraint[1])
+            constraints.half_open_interval(
+                constraint_interval[0], constraint_interval[1]
+            )
         )
         self.uangle = Parameter(
-            self.constr.inv(torch.tensor([angle])), requires_grad=enabled
+            self.constr.inv(torch.tensor([angle])), requires_grad=unlocked
         )
 
     @property
     def angle(self):
         return self.constr(self.uangle)
 
-    def enable(self, enable: bool):
-        self.uangle.requires_grad_(enable)
+    def unlock(self, constraint_interval: Tuple[float, float] = None):
+        if constraint_interval is not None:
+            angle = self.constr(self.uangle)
+            self.constr = dist.transform_to(
+                constraints.half_open_interval(
+                    constraint_interval[0], constraint_interval[1]
+                )
+            )
+            self.uangle.data[:] = self.constr.inv(torch.tensor([angle]))
+        self.uangle.requires_grad_(True)
 
     def matrix(self) -> torch.Tensor:
         raise NotImplementedError
@@ -80,85 +89,115 @@ class Bone(torch.nn.Module):
         self,
         name: str,
         children: Optional[List["Bone"]] = None,
-        t_parent: torch.Tensor = torch.eye(4),
+        t_rest: torch.Tensor = torch.eye(4),
     ):
         super().__init__()
         self.name = name
         self.child_bones = torch.nn.ModuleList(children)
-        self.t_parent = t_parent
+        self.t_rest = t_rest
         self.rot_x = RotX()
         self.rot_y = RotY()
         self.rot_z = RotZ()
 
     def matrix(self):
         return (
-            self.t_parent
+            self.t_rest
             @ self.rot_x.matrix()
             @ self.rot_y.matrix()
             @ self.rot_z.matrix()
         )
 
+    def link_to(self, other: "Bone"):
+        self.child_bones.append(other)
+        return other
 
-def iterate_bones(root: Bone):
-    queue = [(root, torch.eye(4))]
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other: "Bone"):
+        return self.name == other.name
+
+
+def bfs(root: Bone):
+    """Breadth-first search over kinematic starting at root."""
+    queue = [(root, None)]
     while queue:
-        b, t_world = queue.pop(0)
-        t = t_world @ b.matrix()
-        yield b, t
-        queue.extend([(b, t) for b in b.child_bones])
+        b, parent = queue.pop(0)
+        yield b, parent
+        queue.extend([(c, b) for c in b.child_bones])
 
 
-def bone_loss(root: Bone, anchor_dict: Dict[str, torch.Tensor]):
+def fk(root: Bone, root_pose: torch.Tensor = None) -> Dict[Bone, torch.Tensor]:
+    """Computes the forward kinematic poses of each bone."""
+    if root_pose is None:
+        root_pose = torch.eye(4)
+    fk_dict = {}
+    for bone, parent in bfs(root):
+        if parent is None:
+            fk_dict[bone] = root_pose @ bone.matrix()
+        else:
+            fk_dict[bone] = fk_dict[parent] @ bone.matrix()
+    return fk_dict
+
+
+def vanilla_bone_loss(root: Bone, anchor_dict: Dict[Bone, torch.Tensor]):
     loss = 0.0
-    for b, t in iterate_bones(root):
-        if b.name in anchor_dict:
-            loss += ((anchor_dict[b.name] - t[:3, 3]) ** 2).sum()
+    fk_dict = fk(root)
+    for bone, loc in anchor_dict.items():
+        loss += ((loc - fk_dict[bone][:3, 3]) ** 2).sum()
     return loss
 
 
-def solve(root: Bone, anchor_dict: Dict[str, torch.Tensor]):
-
+def solve(root: Bone, anchor_dict: Dict[Bone, torch.Tensor]):
     opt = optim.LBFGS(
         [p for p in root.parameters() if p.requires_grad], history_size=10, max_iter=4
     )
-    for _ in range(5):
+    last_loss = 1e10
+    for e in range(5):
 
         def closure():
             opt.zero_grad()
-            loss = bone_loss(root, anchor_dict)
+            loss = vanilla_bone_loss(root, anchor_dict)
             loss.backward()
             return loss
 
         opt.step(closure)
-        print(bone_loss(root, anchor_dict).item())
+        loss = vanilla_bone_loss(root, anchor_dict).item()
+        if loss >= last_loss or (last_loss - loss) / last_loss < 1e-2:
+            break
+        last_loss = loss
+    print(f"Completed after {e} epochs, loss {loss}")
 
 
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
     b0 = Bone("0")
-    b1 = Bone("1", t_parent=torch.Tensor(T.translation_matrix([0, 1.0, 0])))
-    b2 = Bone("end", t_parent=torch.Tensor(T.translation_matrix([0, 1.0, 0])))
+    b1 = Bone("1", t_rest=torch.Tensor(T.translation_matrix([0, 1.0, 0])))
+    b2 = Bone("end", t_rest=torch.Tensor(T.translation_matrix([0, 1.0, 0])))
 
-    b0.rot_z.enable(True)
-    b1.rot_z.enable(True)
+    b0.rot_z.unlock((-0.5, 0.5))
+    b1.rot_z.unlock()
+    b0.link_to(b1).link_to(b2)
 
-    b0.child_bones.append(b1)
-    b1.child_bones.append(b2)
-
-    a_dict = {"1": torch.Tensor([1.0, 1.0, 0]), "end": torch.Tensor([2.0, 0.0, 0])}
+    a_dict = {b1: torch.Tensor([1.0, 1.0, 0]), b2: torch.Tensor([2.0, 0.0, 0])}
     solve(b0, a_dict)
 
+    # Plot anchors
     fig, ax = plt.subplots()
     for n, loc in a_dict.items():
         ax.scatter([loc[0].item()], [loc[1].item()], c="k", marker="+")
 
-    prev_pos = [0, 0]
-    for b, t in iterate_bones(b0):
-        print(b.name, b.rot_z.angle, t[:3, 3])
-        pos = [t[0, 3].item(), t[1, 3].item()]
-        ax.plot([prev_pos[0], pos[0]], [prev_pos[1], pos[1]], c="green")
-        ax.scatter([pos[0]], [pos[1]], c="green")
-        prev_pos = pos
+    with torch.no_grad():
+        fk_dict = fk(b0)
+        for bone, parent in bfs(b0):
+            print(bone.name, bone.rot_z.angle)
+            tb = fk_dict[bone][:2, 3].numpy()
+            if parent is not None:
+                tp = fk_dict[parent][:2, 3].numpy()
+                ax.plot([tp[0], tb[0]], [tp[1], tb[1]], c="green")
+            ax.scatter([tb[0]], [tb[1]], c="green")
 
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-1, 5)
-    plt.show()
+        ax.set_xlim(-5, 5)
+        ax.set_ylim(-1, 5)
+        plt.show()
