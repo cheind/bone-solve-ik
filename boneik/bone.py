23a184034350391dca36757c3f06c6c53b0028c5
Christoph Heindl <christoph.heindl@gmail.com>
2021-08-18 06:24:37 +0200

    moved code

--- boneik/bone.py
+++ boneik/bone.py
@@ -4,76 +4,8 @@ import torch
 import torch.nn
 import transformations as T
 import torch.optim as optim
-from torch.nn.parameter import Parameter
 
-from .reparametrize import AngleReparametrization
-
-
-class RotDOF(torch.nn.Module):
-    def __init__(
-        self,
-        angle: float = 0.0,
-        constraint_interval: Tuple[float, float] = (-np.pi, np.pi),
-        unlocked: bool = False,
-    ):
-        super().__init__()
-        self.reparam = AngleReparametrization(constraint_interval)
-        self.uangle = Parameter(
-            self.reparam.inv(torch.tensor([angle])), requires_grad=unlocked
-        )
-
-    @property
-    def angle(self):
-        return self.reparam(self.uangle)
-
-    def unlock(self, constraint_interval: Tuple[float, float] = None):
-        if constraint_interval is not None:
-            angle = self.reparam(self.uangle)
-            self.reparam = AngleReparametrization(constraint_interval)
-            self.uangle.data[:] = self.reparam.inv(torch.tensor([angle]))
-        self.uangle.requires_grad_(True)
-
-    def matrix(self) -> torch.Tensor:
-        raise NotImplementedError
-
-
-class RotX(RotDOF):
-    def matrix(self) -> torch.Tensor:
-        a = self.angle
-        c = torch.cos(a)
-        s = torch.sin(a)
-        m = torch.eye(4)
-        m[1, 1] = c
-        m[1, 2] = -s
-        m[2, 1] = s
-        m[2, 2] = c
-        return m
-
-
-class RotY(RotDOF):
-    def matrix(self) -> torch.Tensor:
-        a = self.angle
-        c = torch.cos(a)
-        s = torch.sin(a)
-        m = torch.eye(4)
-        m[0, 0] = c
-        m[0, 2] = s
-        m[2, 0] = -s
-        m[2, 2] = c
-        return m
-
-
-class RotZ(RotDOF):
-    def matrix(self) -> torch.Tensor:
-        a = self.angle
-        c = torch.cos(a)
-        s = torch.sin(a)
-        m = torch.eye(4)
-        m[0, 0] = c
-        m[0, 1] = -s
-        m[1, 0] = s
-        m[1, 1] = c
-        return m
+from .dof import RotX, RotY, RotZ
 
 
 class Bone(torch.nn.Module):
