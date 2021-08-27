d3299e4a21dc563594812551e608186250caa08f
Christoph Heindl <christoph.heindl@gmail.com>
2021-08-27 19:40:11 +0200

    added support for translations

--- boneik/dofs.py
+++ boneik/dofs.py
@@ -17,6 +17,7 @@ class BaseRotDOF(torch.nn.Module):
         super().__init__()
         self.reparam = ConstrainedAngleReparametrization(interval)
         self.uangle = Parameter(self.reparam.angle2log(angle), requires_grad=unlocked)
+        self._reset_value = self.uangle.data.clone()
 
     @property
     def angle(self):
@@ -28,6 +29,10 @@ class BaseRotDOF(torch.nn.Module):
     @torch.no_grad()
     def project_(self):
         self.reparam.project_inplace(self.uangle)
+
+    @torch.no_grad()
+    def reset_(self):
+        self.uangle.data[:] = self._reset_value
 
 
 class RotX(BaseRotDOF):
@@ -60,4 +65,43 @@ class RotZ(BaseRotDOF):
         m[0, 1] = -s
         m[1, 0] = s
         m[1, 1] = c
+        return m
+
+
+class BaseTransDOF(torch.nn.Module):
+    def __init__(
+        self,
+        *,
+        offset: float = 0.0,
+        unlocked: bool = True,
+    ):
+        super().__init__()
+        self.offset = Parameter(torch.tensor(offset), requires_grad=unlocked)
+        self._reset_value = self.offset.data.clone()
+
+    def matrix(self) -> torch.Tensor:
+        raise NotImplementedError
+
+    def reset_(self):
+        self.offset.data.fill_(self._reset_value)
+
+
+class TransX(BaseTransDOF):
+    def matrix(self) -> torch.Tensor:
+        m = torch.eye(4)
+        m[0, 3] = self.offset
+        return m
+
+
+class TransY(BaseTransDOF):
+    def matrix(self) -> torch.Tensor:
+        m = torch.eye(4)
+        m[1, 3] = self.offset
+        return m
+
+
+class TransZ(BaseTransDOF):
+    def matrix(self) -> torch.Tensor:
+        m = torch.eye(4)
+        m[2, 3] = self.offset
         return m
